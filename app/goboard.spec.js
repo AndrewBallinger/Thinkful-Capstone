7f2c3a0abbcedb50698d3b6511674afa2ea5bf92
Andrew Ballinger <aballinger@payjunction.com>
2016-08-23 16:31:23 -0700

    Another ream for an index.html (not finished)

--- app/goboard.spec.js
+++ app/goboard.spec.js
@@ -3,12 +3,18 @@ describe('app', () => {
   var scope;
   
   beforeEach(module('goboardApp'));
-
+  beforeEach(module('index.html'));
+  
   beforeEach(inject(($rootScope, $compile) => {
     scope = $rootScope.$new();
+    
   }));
   
-  it('Does not fall over when simply requiring the base namespace', () => {
+  it('does not fall over when simply requiring the base namespace', () => {
+    expect(scope).toBeTruthy();
+  });
+
+  it('does not fall over when simply requiring the index.html', () => {
     expect(scope).toBeTruthy();
   });
 });
