03b9ce0a45fac9ae9f26ef02ea50130faa4618c4
Andrew Ballinger <andrewb@procore.com>
2016-08-25 21:08:07 -0700

    Core functionality in place

--- app/factories/goboard-state.js
+++ app/factories/goboard-state.js
@@ -1,51 +1,60 @@
 /* global angular _ Immutable firebase */
 angular.module('goboardFactories')
-  .factory('goboardState', function($firebaseAuth, $firebaseArray, $rootScope, CONSTANTS, goboardLogic) { 
-    var state = {};
-    var moves = [];
+  .factory('goboardState', function ($firebaseAuth, $firebaseArray, $rootScope, CONSTANTS, goboardLogic) {
+    var state = {}
+    var moves = []
 
-    var to_move = CONSTANTS.PIECE.BLACK;
-    var blank_position = Immutable.Map();
-    var last_position = Immutable.Map();
+    var blank_position = Immutable.Map()
+    var last_position = Immutable.Map()
 
     var auth = $firebaseAuth();
-    var signin = () => (auth.$signInAnonymously().catch((err) => { console.error(err); signin()}));
-    signin();
-    
-    firebase.auth().onAuthStateChanged( function(user) {
-      var ref = firebase.database().ref().child("development");
-      moves = $firebaseArray(ref);
+    var signin = () => {
+      try {
+        auth.$signInAnonymously()
+      } catch (err) {
+        console.error(err)
+        signin()
+      }
+    }
+    signin()
+
+    firebase.auth().onAuthStateChanged(function (user) {
+      var ref = firebase.database().ref().child('development')
+      moves = $firebaseArray(ref)
 
       $rootScope.$watchCollection(moves, () => {
-        last_position = _.reduce(moves, (acc, move) => { return goboardLogic.applyMove(acc,move) }, blank_position);
-      });
-      
-    });
+        last_position = _.reduce(moves, (acc, move) => { return goboardLogic.applyMove(acc,move) }, blank_position)
+      })
+
+      moves.$watch(() => {
+        last_position = _.reduce(moves, (acc, move) => { return goboardLogic.applyMove(acc,move) }, blank_position)
+      })
+    })
 
     state.lookupPiece = (space) => {
-      return goboardLogic.lookupPiece(last_position, space);
+      return goboardLogic.lookupPiece(last_position, space)
     }
 
     state.placePiece = (space) => {
-      var move = {time: new Date(), row: parseInt(space.row), column: parseInt(space.column), piece: to_move };
-      if ( !goboardLogic.moveIsValid(last_position, move) ) return;
+      var to_move = state.nextMove()
 
-      to_move = (to_move === CONSTANTS.PIECE.BLACK) ? CONSTANTS.PIECE.WHITE : CONSTANTS.PIECE.BLACK;
-      moves.$add(move);
-      last_position = goboardLogic.applyMove(last_position, move);
+      var move = {time: new Date(), row: parseInt(space.row), column: parseInt(space.column), piece: to_move }
+      if (!goboardLogic.moveIsValid(last_position, move)) return
+
+      moves.$add(move)
+      last_position = goboardLogic.applyMove(last_position, move)
     }
-    
+
     state.clearBoard = (spaces) => {
-      to_move = CONSTANTS.PIECE.BLACK;
       blank_position = Immutable.Map(
-        _.reduce(spaces, (m, s) => { m[goboardLogic.moveKey(s)] = CONSTANTS.PIECE.EMPTY; return m }, {} ));
-      last_position = blank_position;
-      console.log("State is tracking " + last_position.size + " spaces");
+        _.reduce(spaces, (m, s) => { m[goboardLogic.moveKey(s)] = CONSTANTS.PIECE.EMPTY; return m }, {}))
+      last_position = blank_position
+      console.log('State is tracking ' + last_position.size + ' spaces')
     }
-    
-    state.blackToMove = () => to_move === CONSTANTS.PIECE.BLACK;
 
+    state.blackToMove = () => state.nextMove() === CONSTANTS.PIECE.BLACK
 
+    state.nextMove = () => moves.length % 2 === 0 ? CONSTANTS.PIECE.BLACK : CONSTANTS.PIECE.WHITE
 
-    return state;
+    return state
   });
